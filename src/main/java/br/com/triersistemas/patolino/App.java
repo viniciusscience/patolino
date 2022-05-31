670a74f8824e06c7d34ea8da87d2ae94f4422c13
vinicius.silva <daftwarz@gmail.com>
2022-05-31 11:32:36 -0300

    Projetopatolino

---
+++ src/main/java/br/com/triersistemas/patolino/App.java
@@ -0,0 +1,21 @@
+package br.com.triersistemas.patolino;
+
+import java.util.Arrays;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        Farmacêutico a = new Farmacêutico();
+        a.getOfertaDias();
+        System.out.println(a.getOfertaDias());
+        a.getNome();
+        System.out.println(a.getNome());
+        Fornecedor ta = new Fornecedor();
+        ta.getProduto();
+        System.out.println(ta.getProduto());
+        a.getDocumento();
+        
+    }
+}
